a5ccb6f8c19268f6d086c1826e33541b9ea74c2c
Marián Hollý <marian.holly7117@gmail.com>
2025-11-09 12:04:57 +0100

    state management in nextjs

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,7 +1,26 @@
+"use client";
+
+import { useState } from "react";
+
+interface MovieData {
+  title: string;
+  year: number;
+  watched_date: string;
+  rating: number | null;
+  tmdb_title: string;
+  poster: string | null;
+  overview: string;
+  tmdb_rating: number;
+  release_date: string;
+}
+
 export default function Home() {
+  const [file, setFile] = useState<File | null>(null);
+  const [loading, setLoading] = useState(false);
+  const [movie, setMovie] = useState<MovieData | null>(null);
+  const [error, setError] = useState<string | null>(null);
+
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-   
-    </div>
+    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black"></div>
   );
 }
